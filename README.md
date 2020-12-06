42cf00462b007f84b2f99d883009b46b0c92d0d7
vihangwhite <75125449+vihangwhite@users.noreply.github.com>
2020-12-06 13:09:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mylibrary
+# collisionAlgorithm
+Algorithm to detect collision between two objects
---
+++ sketch.js
@@ -0,0 +1,60 @@
+var fixedRect, movingRect;
+var gameObject1,gameObject2;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  gameObject1 = createSprite(100,100,50,50);
+  gameObject1.shapeColor = "green";
+  gameObject2 = createSprite(200,100,50,50);
+  gameObject2.shapeColor = "green";
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+  bounceOff(movingRect,fixedRect);
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+if(isTouching(movingRect,gameObject1)) {
+  movingRect.shapeColor = "red";
+  gameObject1.shapeColor = "red";
+
+}
+else{
+  movingRect.shapeColor = "green";
+  gameObject1.shapeColor = "green";
+}
+  
+  drawSprites();
+}
+function isTouching(object1,object2) {
+  if (object1.x-object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2) {
+  return true;
+}
+else {
+  return false;
+}
+}
+function bounceOff(object1,object2) {
+  if (object1.x-object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2) {
+  object1.velocityX = object1.velocityX * (-1);
+  object2.velocityX = object2.velocityX * (-1);
+}
+if( object1.y - object2.y < object2.height/2 + object1.height/2
+&& object2.y - object1.y < object2.height/2 + object1.height/2) {
+  object1.velocityY = object1.velocityY * (-1);
+  object2.velocityY = object2.velocityY * (-1);
+}
+}
